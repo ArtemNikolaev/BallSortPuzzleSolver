174b58509375776ff586f4435a7b545050544381
Artem Nikolaev <artem@nikolaev.by>
2021-01-08 23:34:44 +0300

    tests for isSolved

---
+++ dist/__tests__/isSolved.js
@@ -0,0 +1,55 @@
+const isSolved = require('../isSolved');
+
+describe('isSolved', () => {
+  it('пустые и полные колбы', () => {
+    const data = [
+      [],
+      ['2', '2', '2'],
+      [],
+      ['4', '4', '4'],
+    ];
+
+    expect(isSolved(data)).toBe(true);
+  });
+
+  describe('цвет повторяется', () => {
+    const dataArr = [
+      [
+        [],
+        ['2', '2', '2' ],
+        ['2', '2', '2' ],
+        ['4', '4', '4'],
+      ],
+      [
+      [],
+        ['2', '2', '2' ],
+        ['2' ],
+        ['4', '4', '4'],
+      ]
+    ];
+
+    dataArr.forEach(data =>
+      expect(isSolved(data)).toBe(false)
+    );
+  });
+  describe('колба заполнена больше чем одним цветом', () => {
+    const dataArr = [
+      [
+        [],
+        ['2', '2', '2'],
+        ['1', '3', '3'],
+        ['4', '4', '4'],
+      ],
+      [
+        [],
+        ['2', '2', '2'],
+        ['4', '3', '3'],
+        ['4', '4', '4'],
+      ],
+    ];
+
+    dataArr.forEach(data =>
+      expect(isSolved(data)).toBe(false)
+    );
+  });
+});
---
+++ dist/isSolved.js
@@ -0,0 +1,3 @@
+module.exports =  function isSolved(state) {
+  return true;
+}
